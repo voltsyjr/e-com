beee79f176f58790dc4e48738ff98fca3be85c0f
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2022-05-21 18:17:18 +0530

    .

--- src/std_com/Navigation/BottomNav/BottomNav.tsx
+++ src/std_com/Navigation/BottomNav/BottomNav.tsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./BottomNav.scss";
 import { motion } from "framer-motion";
 
+/*
+  notes 
+    1. notice the lastScroll in the useEffect, its state is saved in the function that is recorded by the window object 
+    2. notice the conditional return of jsx from the function
+*/
+
 function BottomNav() {
   const [showBottomNav, setShowBottomNav] = useState(true);
   useEffect(() => {
@@ -13,12 +19,12 @@ function BottomNav() {
       } else if (lastScroll - window.scrollY >= thresh) {
         setShowBottomNav(false);
       }
-      
       lastScroll =
         Math.abs(window.scrollY - lastScroll) > thresh
           ? window.scrollY
           : lastScroll;
     };
+    return () => {window.onscroll = () => {}}; 
   }, []);
 
   if (showBottomNav)
@@ -27,7 +33,9 @@ function BottomNav() {
         className="navContainer"
         animate={showBottomNav ? { bottom: 0 } : {}}
         transition={{ duration: 0.3 }}
-      ></motion.div>
+      >
+
+      </motion.div>
     );
   else return <></>;
 }
